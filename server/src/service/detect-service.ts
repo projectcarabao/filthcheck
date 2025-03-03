74de77ee4787172bd63572b59d81f9e1b4754edf
josephencila <joseph.mesia.encila@gmail.com>
2025-03-03 11:37:20 +0800

    feat: detect service

---
+++ server/src/service/detect-service.ts
@@ -0,0 +1,18 @@
+
+import { Request, Response } from "express"
+import nsfwDetector from "@/utils/nsfw-detector";
+
+export const signInWithEmailService = async (request: Request, response: Response) => {
+    try {
+
+        const { imageURL } = request.params
+
+        const response = await nsfwDetector(imageURL);
+
+        return { status: 200, data: response };
+
+    } catch (error) {
+
+        if (error instanceof Error) throw new Error(error.message);
+    }
+}
