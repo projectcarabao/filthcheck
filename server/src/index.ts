8081bea2bfeed349f231d10381aa2b83b4c30833
josephencila <joseph.mesia.encila@gmail.com>
2025-03-03 13:40:37 +0800

    chore: clean index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,8 +5,8 @@ import helmet from 'helmet'
 import rateLimit from 'express-rate-limit'
 import morgan from 'morgan'
 import dotenv from 'dotenv'
+
 import detectRoutes from '@/routes/detect-routes'
-import nsfwDetector from '@/utils/nsfw-detector'
 
 dotenv.config()
 
@@ -37,19 +37,6 @@ app.use(cookieParser());
 // app.use(limiter);
 
 app.use('/api/detect', detectRoutes)
-
-app.get("/", async (req, res) => {
-    try {
-        const wait = 'https://dthezntil550i.cloudfront.net/v3/latest/v32301150603333930008587190/1280_960/c979ecc1-f5d7-4093-8faf-6f879b7d08e6.png'
-        const response = await nsfwDetector(wait)
-        res.status(200).json({ data: response })
-    } catch (error) {
-        if (error instanceof Error) {
-            console.error(error);
-            res.status(400).json({ error: "Image processing failed" });
-        }
-    }
-})
 
 const port = NODE_PORT || 5000
 app.listen(port, () => {
